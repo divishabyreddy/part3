debb5eb5d708fbdfe33eb7c6ba09a8107cb0d325
Byreddy <db185289@ncr.com>
2020-05-08 15:46:31 +0530

    added build

---
+++ build/precache-manifest.6707c24095d89223ba5287a432caae59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d92e98b74287f41180d0fe69f03aee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a4a10ebfad52ec81397",
+    "url": "/static/css/main.e91c0fd9.chunk.css"
+  },
+  {
+    "revision": "10d338e68a0d8a295b88",
+    "url": "/static/js/2.c45c264f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c45c264f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4a10ebfad52ec81397",
+    "url": "/static/js/main.67874b0e.chunk.js"
+  },
+  {
+    "revision": "d540d796d2993bde2c1a",
+    "url": "/static/js/runtime-main.17f5897d.js"
+  }
+]);
